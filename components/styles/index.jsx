ec3d87694db858ffe4a1dae05eb66e11bdc8d8f3
Aakash Bhatia <akash.bhatia1184@gmail.com>
2019-12-28 20:33:00 +1100

    CSS resets and normalise implemented

--- components/styles/index.jsx
+++ components/styles/index.jsx
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import "../../node_modules/normalize.css/normalize.css";
+import "../../static/css/resets.css";
 import { Raleway, Roboto } from "./fontEmbedding/";
 import Elements from "./elements";
 
