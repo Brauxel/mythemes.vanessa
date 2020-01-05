b9d4aa90642f37ebeb3585680778b797bcefd8f2
Aakash Bhatia <akash.bhatia1184@gmail.com>
2020-01-05 16:09:20 +1100

    <Text> back to normal and drill props no longer filtered NOTE: need to run the attributes past and SEO person

--- components/atoms/Text/styles.jsx
+++ components/atoms/Text/styles.jsx
@@ -1,14 +1,9 @@
 import styled from "styled-components";
 
-const styles = styled(props => {
-	// We've disabled linting for the next line but remember to not make this the norm, the line below is an edge case
-	const { color, textAlign, fontFamily, ...drillProps } = props; // eslint-disable-line
-
-	return <p {...drillProps} />;
-})`
-    color ${props => props.color || "inherit"};
-    text-align: ${props => props.textAlign || "inherit"};
-    font-family: ${props => props.fontFamily || "inherit"};
+const styles = styled.p`
+	color ${props => props.color || "inherit"};
+	text-align: ${props => props.textAlign || "inherit"};
+	font-family: ${props => props.fontFamily || "inherit"};
 `;
 
 export default styles;
